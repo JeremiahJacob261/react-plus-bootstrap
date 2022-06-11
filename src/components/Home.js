d3fe7bc2a611e6e0d257619b40ed1981740d3717
JeremiahJacob261 <jeremiahjacob261@gmail.com>
2022-06-11 23:11:14 +0100

    2 files modified

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,20 +20,19 @@ class Home extends React.Component {
   render(){
 return(
 <div class="home">
- <h2>Home</h2>
  <Container>
  <div id="home-image-top">
   <img id="top" src="https://cdn.mos.cms.futurecdn.net/yL3oYd7H2FHDDXRXwjmbMf.jpg"/>
   </div>
   <Row class="home-start">
-  <Col class="home-start-text" sm={8}>
+  <Col class="home-start-text" sm={6}>
   <h2>Want to learn How to Light, Shoot & Colorgrade In-Studio Portraits like Amazing Klef?? This is for you!!</h2>
   <p>A Comprehensive Video Course designed by Amazing Klef to help improve your studio lighting skills and empower you to create clean & creative images.
 
 This course will take you from "Amateur" to "Beast" in the studio using simple tricks and setups.</p>
 <Button variant="dark">Get Started!</Button>
   </Col>
-  <Col class="home-image-side-col">
+  <Col class="home-image-side-col" sm={5}>
   <div id="home-image-side">
   <img id="side" src="https://cdn.mos.cms.futurecdn.net/yL3oYd7H2FHDDXRXwjmbMf.jpg"/>
   </div>
