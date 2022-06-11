4279409148c05fe40a65203aac32e683a460c968
JeremiahJacob261 <jeremiahjacob261@gmail.com>
2022-06-12 00:02:11 +0100

    2 files modified

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,48 @@
 import React from 'react';
-function Products(){
+import {render} from 'react-dom';
+import {Dropdown} from 'react-bootstrap'
 
+class Products extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+render(){
+  return(
+    <div>
+    <div class="Products-div"> 
+<h1>Products</h1>
+</div>
+<div class="filter-box">
+  Filter By
+<Dropdown>
+  <Dropdown.Toggle variant="light" id="dropdown-basic">
+    All Products
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+Sort By
+<Dropdown>
+  <Dropdown.Toggle variant="light" id="dropdown-basic">
+    Alphabetically A-Z
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Best Selling</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Featured </Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Alphabetically</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+</div>
+    </div>
+  );
+}
 }
 export default Products;
