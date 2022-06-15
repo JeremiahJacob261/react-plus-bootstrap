26b0687481620ac889651470c71e75a2b8c64d35
JeremiahJacob261 <jeremiahjacob261@gmail.com>
2022-06-15 18:04:52 +0100

    2 files modified

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -4,51 +4,38 @@ import {render} from 'react-dom';
 import {Form,Button,File} from 'react-bootstrap';
 import { collection, addDoc , doc, setDoc } from "firebase/firestore";
 import '../style.css';
-import { ref } from "firebase/storage";
+
       function AdminHome (){
        
-         const[product,setProduct]=useState();
-         const[price,setPrice]=useState();
-         const [imageUpload ,setImageUpload]=useState(null);
-          const Lick= async()=>{
-            /*slammy is a collection */
-            const imageRef = ref(storage, `slammy/${imageUpload.name + v4()}`);
-            uploadBytes(imageRef, imageUpload).then((snapshot) => {
-              getDownloadURL(snapshot.ref).then((url) => {
-                setImageUrls((prev) => [...prev, url]);
-              });
-            });
-          
-            if(imageUpload == null){return;}
-            await addDoc(collection(db, "slammy"), {
-              product: product,
-              price: price,
-            });
+        const[product,setProduct]=useState();
+        const[price,setPrice]=useState();
+        const [imageUpload ,setImageUpload]=useState(null);
+         const Lick= async()=>{
+           /*slammy is a collection */
+           await addDoc(collection(db, "slammy"), {
+             product: product,
+             price: price,
+           });
 
-          }
-          return(
-            <div>
-            <h2>Add Products</h2>
-            <Form class="product-info">
-              Select picture
-    <input type="file" onChange={
-  (imageUpload) => {
-    setImageUpload(imageUpload.target.files[0]);
-    }}/>
-    <Form.Group className="mb-3" controlId="name">
-      <Form.Control type="text" value={product} onChange={(product) => {
-            setProduct(product.target.value);
-          }} placeholder="product name"/>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="price">
-      <Form.Control type="number" value={price} onChange={(price) => {
-            setPrice(price.target.value);
-          }} placeholder="price in dollars" />
-      </Form.Group>
-      <Button variant='secondary' onClick={Lick}>Add Product</Button>
-      </Form>
-              </div>
-            );
+         }
+         return(
+           <div>
+           <h2>Add Products</h2>
+           <Form class="product-info">
+   <Form.Group className="mb-3" controlId="name">
+     <Form.Control type="text" value={product} onChange={(product) => {
+           setProduct(product.target.value);
+         }} placeholder="product name"/>
+     </Form.Group>
+     <Form.Group className="mb-3" controlId="price">
+     <Form.Control type="number" value={price} onChange={(price) => {
+           setPrice(price.target.value);
+         }} placeholder="price in dollars" />
+     </Form.Group>
+     <Button variant='secondary' onClick={Lick}>Add Product</Button>
+     </Form>
+             </div>
+           );
         }
        
       
